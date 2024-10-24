0b763808d8fe731e69bdc14d6ab56b43329d24f5
Max Mustermann <max.megamustermann@gmail.com>
2024-10-24 20:54:16 +0200

    Add test for search view

--- backend/stocks/apps.py
+++ backend/stocks/apps.py
@@ -1,10 +1,25 @@
+import os
+import threading
+
 from django.apps import AppConfig
 
+background_thread = None
 
-class StockConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+
+class StocksConfig(AppConfig):
     name = "stocks"
+    default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
         import stocks.signals  # noqa F401
-        import stocks.tasks  # noqa F401
+
+        from .tasks import stock_updater
+
+        global background_thread
+        background_thread = threading.Thread(target=stock_updater, daemon=True)
+
+        current_settings_module = os.environ.get("DJANGO_SETTINGS_MODULE")
+        alive = background_thread.is_alive()
+        if current_settings_module != "your_project.settings.test" and not alive:
+            background_thread.start()
+            print("Background Thread started")
