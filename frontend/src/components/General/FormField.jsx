fe15af000736e9c3fd5cf184a7e1cd71b2197536
Max Mustermann <max.megamustermann@gmail.com>
2025-01-29 15:16:19 +0100

    Add edit view to profile page

--- frontend/src/components/General/FormField.jsx
+++ frontend/src/components/General/FormField.jsx
@@ -1,7 +1,19 @@
-const FormField = ({ label, type, name, value, onChange, onBlur, error, width }) => {
+const FormField = ({
+    label,
+    type,
+    name,
+    value,
+    onChange,
+    onBlur,
+    error,
+    width = "col-12",
+    showError = true,
+}) => {
     return (
-        <div className={`form-group ${width ? width : 'col-12'}`}>
-            <label className="form-label" htmlFor={name}>{label}</label>
+        <div className={`form-group ${width}`}>
+            <label className="form-label" htmlFor={name}>
+                {label}
+            </label>
             <input
                 type={type}
                 id={name}
@@ -9,10 +21,16 @@ const FormField = ({ label, type, name, value, onChange, onBlur, error, width })
                 value={value}
                 onChange={onChange}
                 onBlur={onBlur}
-                className={`form-control ${error ? 'is-invalid' : 'is-valid'}`}
+                className={`form-control ${
+                    showError && (error ? "is-invalid" : "is-valid")
+                }`}
             />
-            <div className="invalid-feedback">{error}</div>
-            <div className="valid-feedback">Sieht gut aus!</div>
+            {showError && (
+                <>
+                    <div className="invalid-feedback">{error}</div>
+                    <div className="valid-feedback">Sieht gut aus!</div>
+                </>
+            )}
         </div>
     );
 };
--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -1,23 +1,168 @@
-import React from 'react';
-import ProfileNavigation from '../../components/Navigation/ProfileNavigation';
-import Area from '../../components/General/Area';
+import React from "react";
+import ProfileNavigation from "../../components/Navigation/ProfileNavigation";
+import Area from "../../components/General/Area";
+import FormField from "../../components/General/FormField";
 
 function Profile() {
-    return <>
-        <ProfileNavigation />
-        <Area title="Profil" key1="profile">
-            {({ value: profile }) => <>
-                <p className="fs-5 mt-3">
-                    Nutzername: {profile.user.username}<br />
-                    Teamname: {profile.team}<br />
-                    Vorname: {profile.user.first_name}<br />
-                    Nachname: {profile.user.last_name}<br />
-                    E-Mail: {profile.user.email}<br />
-                </p>
-                <a href="/logout" className="btn btn-primary">Logout</a>
-            </>}
-        </Area>
-    </>
+    const [isEditing, setIsEditing] = React.useState(false);
+    const [formData, setFormData] = React.useState({
+        username: "",
+        first_name: "",
+        last_name: "",
+    });
+
+    const handleEdit = (profile) => {
+        setFormData({
+            username: profile.user.username,
+            first_name: profile.user.first_name,
+            last_name: profile.user.last_name,
+        });
+        setIsEditing(true);
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSave = () => {
+        setIsEditing(false);
+    };
+
+    return (
+        <>
+            <ProfileNavigation />
+            <Area title="Profil" key1="profile">
+                {({ value: profile }) => (
+                    <>
+                        {isEditing ? (
+                            <form className="row g-3 mb-5">
+                                <FormField
+                                    label="Benutzername"
+                                    type="text"
+                                    name="username"
+                                    value={formData.username}
+                                    onChange={handleChange}
+                                    showError={false}
+                                />
+                                <FormField
+                                    label="Vorname"
+                                    type="text"
+                                    name="first_name"
+                                    value={formData.first_name}
+                                    onChange={handleChange}
+                                    showError={false}
+                                    width="col-md-6"
+                                />
+                                <FormField
+                                    label="Benutzername"
+                                    type="text"
+                                    name="last_name"
+                                    value={formData.last_name}
+                                    onChange={handleChange}
+                                    showError={false}
+                                    width="col-md-6"
+                                />
+                                {/*<div className="my-3">
+                                    <label
+                                        htmlFor="username"
+                                        className="form-label"
+                                    >
+                                        Benutzername
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="username"
+                                        value={formData.username}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col">
+                                        <label
+                                            htmlFor="firstname"
+                                            className="form-label"
+                                        >
+                                            Vorname
+                                        </label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="firstname"
+                                            value={formData.first_name}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="col">
+                                        <label
+                                            htmlFor="lastname"
+                                            className="form-label"
+                                        >
+                                            Nachname
+                                        </label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="lastname"
+                                            value={formData.last_name}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>*/}
+                            </form>
+                        ) : (
+                            <p className="fs-5 mt-3">
+                                Nutzername: {profile.user.username}
+                                <br />
+                                Teamname: {profile.team}
+                                <br />
+                                Vorname: {profile.user.first_name}
+                                <br />
+                                Nachname: {profile.user.last_name}
+                                <br />
+                                E-Mail: {profile.user.email}
+                                <br />
+                            </p>
+                        )}
+                        <div className="d-flex justify-content-between">
+                            {isEditing ? (
+                                <>
+                                    <button
+                                        type="button"
+                                        className="btn btn-success"
+                                        onClick={handleSave}
+                                    >
+                                        Speichern
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-danger"
+                                        onClick={() => setIsEditing(false)}
+                                    >
+                                        Abbrechen
+                                    </button>
+                                </>
+                            ) : (
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    onClick={() => handleEdit(profile)}
+                                >
+                                    Bearbeiten
+                                </button>
+                            )}
+                            <a href="/logout" className="btn btn-primary">
+                                Logout
+                            </a>
+                        </div>
+                    </>
+                )}
+            </Area>
+        </>
+    );
 }
 
-export default Profile
+export default Profile;
